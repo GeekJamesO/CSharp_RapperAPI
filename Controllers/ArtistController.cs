6100e3008bb2000cd67e77b8df5462f3a903db93
JamesORourke <geekjamesorourkejr@icloud.com>
2017-10-10 14:50:16 -0700

    done

--- Controllers/ArtistController.cs
+++ Controllers/ArtistController.cs
@@ -34,5 +34,50 @@ namespace MusicApi.Controllers {
             instructions += "       *ListArtists=?(true/false)\n";
             return instructions;
         }
+
+        // Create a route for /artists that returns all artists as JSON        
+        [Route("artists")]
+        [HttpGet]
+        public JsonResult root()
+        {
+            List<Artist> result = allArtists.OrderBy(k => k.ArtistName).ToList();
+            return Json(result);
+        }
+
+        // Create a route /artists/name/{name} that returns all artists that match the provided name
+        [Route("artists/name/{name}")]
+        [HttpGet]
+        public JsonResult artistsByName(string name) 
+        {
+            List<Artist> result = allArtists.Where(k => k.ArtistName.Contains(name)).OrderBy(k => k.ArtistName).ToList();
+            return Json(result);
+        }
+
+        // Create a route /artists/realname/{name} that returns all artists who real names match the provided name
+        [Route("artists/realname/{name}")]
+        [HttpGet]
+        public JsonResult artistsByRealname(string name)
+        {
+            List<Artist> result = allArtists.Where(k => k.RealName.Contains(name)).OrderBy(k => k.RealName).ToList();
+            return Json(result);
+        }
+
+        // Create a route /artists/hometown/{town} that returns all artists who are from the provided town
+        [Route("artists/hometown/{town}")]
+        [HttpGet]
+        public JsonResult artistsByHometown(string town) 
+        {
+            List<Artist> result = allArtists.Where( k => k.Hometown.Contains(town)).OrderBy(m => m.Hometown).ToList();
+            return Json(result);
+        }
+
+        // Create a route /artists/groupid/{id} that returns all artists who have a GroupId that matches id
+        [Route("/artists/groupid/{id}")]
+        [HttpGet]
+        public JsonResult artistsByGroupId(int id)
+        {
+            List<Artist> result = allArtists.Where(k => k.GroupId == id).OrderBy(m => m.GroupId).ToList();
+            return Json(result);
+        }
     }
 }
--- Controllers/GroupController.cs
+++ Controllers/GroupController.cs
@@ -3,12 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using JsonData;
 
-namespace MusicApi.Controllers {
-    public class GroupController : Controller {
-        List<Group> allGroups {get; set;}
-        public GroupController() {
+namespace MusicApi.Controllers
+{
+    public class GroupController : Controller
+    {
+        List<Group> allGroups { get; set; }
+        List<Artist> allArtists { get; set; }
+
+        public GroupController()
+        {
             allGroups = JsonToFile<Group>.ReadJson();
+            allArtists = JsonToFile<Artist>.ReadJson();
         }
 
+        // Create a route /groups that returns all groups as JSON
+        [Route("groups/{members?}")]
+        [HttpGet]
+        public JsonResult getAllGroups(bool members = false)
+        {
+                var result = allGroups.OrderBy(a => a.GroupName).ToList();
+                if (members) 
+                {
+                    foreach (var g in result) 
+                    {
+                        g.Members = allArtists.Where(a => a.GroupId == g.Id).ToList();
+                    }
+                }
+                return Json(result);
+        }
+
+        // Create a route /groups/name/{name} that returns all groups that match the provided name
+        [Route("groups/name/{name}/{members?}")]
+        [HttpGet]
+        public JsonResult groupsByName(string name, bool members = false)
+        {
+            List<Group> result = allGroups.Where(b => b.GroupName.Contains(name)).OrderBy(a => a.GroupName).ToList();
+            if (members)
+            {
+                foreach (var g in result)
+                {
+                    g.Members = allArtists.Where(a => a.GroupId == g.Id).ToList();
+                }
+            }
+            return Json(result);
+        }
+
+        // Create a route /groups/id/{id} that returns all groups with the provided Id value
+        [Route("groups/id/{id}/{members?}")]
+        [HttpGet]
+        public JsonResult groupsById(int id, bool members = false)
+        {
+            var result = allGroups.Where(b => b.Id == id).ToList();
+            if (members)
+            {
+                foreach (var g in result)
+                {
+                    g.Members = allArtists.Where(a => a.GroupId == g.Id).ToList();
+                }
+            }
+            return Json(result);
+        }
+
+        // (Optional) Add an extra boolean parameter to the group routes called displayArtists that will include members for all Group JSON responses
     }
 }
